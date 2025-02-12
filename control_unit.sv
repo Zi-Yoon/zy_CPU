2e247cf55f7927bdd976f670dfbbbee2b22158ee
Zi-Yoon <1127abj@gmail.com>
2025-02-12 18:34:23 +0900

    Use Header file

--- control_unit.sv
+++ control_unit.sv
@@ -6,6 +6,7 @@ module control_unit (
     input [OPCODE-1:0] i_opcode, // instr[ 6: 0]
     input [FUNCT3-1:0] i_funct3, // instr[14:12]
     input [FUNCT7-1:0] i_funct7, // instr[31:25]
+    output reg o_reg_read,
     output reg o_reg_write,
     output reg o_mem_read,
     output reg o_mem_write,
@@ -17,76 +18,99 @@ module control_unit (
 
     always_comb begin
         // Default values
-        o_reg_write = 1'b0;
-        o_mem_read  = 1'b0;
-        o_mem_write = 1'b0;
-        o_branch    = 1'b0;
-        o_jump      = 1'b0;
-        o_alu_op    = 4'b0;
-        o_imm_sel   = 2'b0;
+        o_reg_write = FALSE;
+        o_mem_read  = FALSE;
+        o_mem_write = FALSE;
+        o_branch    = FALSE;
+        o_jump      = FALSE;
+        o_alu_op    = FALSE;
+        o_imm_sel   = FALSE;
 
         case(i_opcode)
             // ------------------------- R-type -------------------------
             7'b0110011: begin // R-type
-                o_reg_write = 1'b1; // Write to RD
+                o_reg_write = TRUE; // Write Register
                 case({i_funct3, i_funct7})
-                    {7'b0000000, 3'b000}: o_alu_op = 4'b0000; // ADD
-                    {7'b0100000, 3'b000}: o_alu_op = 4'b0001; // SUB
-                    {7'b0000000, 3'b111}: o_alu_op = 4'b0010; // AND
-                    {7'b0000000, 3'b110}: o_alu_op = 4'b0011; // OR
-                    {7'b0000000, 3'b100}: o_alu_op = 4'b0100; // XOR
-                    {7'b0000000, 3'b001}: o_alu_op = 4'b0101; // SLL
-                    {7'b0000000, 3'b101}: o_alu_op = 4'b0110; // SRL
-                    {7'b0100000, 3'b101}: o_alu_op = 4'b0111; // SRA
-                    default: o_alu_op = 4'bxxxx; // ERROR
+                    {7'b0000000, 3'b000}: o_alu_op = ALU_ADD; // ADD
+                    {7'b0100000, 3'b000}: o_alu_op = ALU_SUB; // SUB
+                    {7'b0000000, 3'b111}: o_alu_op = ALU_AND; // AND
+                    {7'b0000000, 3'b110}: o_alu_op = ALU_OR ; // OR
+                    {7'b0000000, 3'b100}: o_alu_op = ALU_XOR; // XOR
+                    {7'b0000000, 3'b001}: o_alu_op = ALU_SLL; // SLL
+                    {7'b0000000, 3'b101}: o_alu_op = ALU_SRL; // SRL
+                    {7'b0100000, 3'b101}: o_alu_op = ALU_SRA; // SRA
+                    default: o_alu_op = ALU_X;                // ERROR
                 endcase
             end
 
             // ------------------------- I-type -------------------------
-            7'b0010011: begin // I-type
-                o_reg_write = 1'b1;  // Write to RD
-                o_imm_sel   = 2'b00; // I-type Immediate
+            7'b0010011: begin
+                o_reg_write = TRUE;  // Write Register
+                o_imm_sel   = IMM_I; // I-type Immediate
                 case(i_funct3)
-                    3'b000: o_alu_op = 4'b0000; // ADDI
-                    3'b111: o_alu_op = 4'b0010; // ANDI
-                    3'b110: o_alu_op = 4'b0011; // ORI
-                    3'b100: o_alu_op = 4'b0100; // XORI
-                    // 3'b001: o_alu_op = 4'b0101; // SLLI
-                    // 3'b101: o_alu_op = 4'b0110; // SRLI
-                    // 3'b101: o_alu_op = 4'b0111; // SRAI
-                    default: o_alu_op = 4'bxxxx; // ERROR
+                    3'b000: o_alu_op = ALU_ADD; // ADDI
+                    3'b111: o_alu_op = ALU_AND; // ANDI
+                    3'b110: o_alu_op = ALU_OR ; // ORI
+                    3'b100: o_alu_op = ALU_XOR; // XORI
+                    default: o_alu_op = ALU_X ; // ERROR
                 endcase
             end
 
-            // --------------------------- LW ---------------------------
-            7'b0000011: begin // LOAD
-                o_mem_read  = 1'b1;    // Read Memory
-                o_reg_write = 1'b1;    // Write Register
-                o_imm_sel   = 2'b00;   // I-type Immediate
-                o_alu_op    = 4'b0000; // rs1 + imm
+            // ------------------------- I-type (LW) --------------------
+            7'b0000011: begin
+                o_mem_read  = TRUE;    // Read Memory
+                o_reg_write = TRUE;    // Write Register
+                o_imm_sel   = IMM_I;   // I-type Immediate
+                o_alu_op    = ALU_ADD; // rs1 + imm
             end
 
             // ------------------------- S-type -------------------------
-            7'b0100011: begin // STORE
-                o_reg_read  = 1'b1;    // Read Register
-                o_mem_write = 1'b1;    // Write Memory
-                o_imm_sel   = 2'b01;   // S-type Immediate
-                o_alu_op    = 4'b0000; // rs1 + imm
+            7'b0100011: begin
+                o_reg_read  = TRUE;    // Read Register
+                o_mem_write = TRUE;    // Write Memory
+                o_imm_sel   = IMM_S;   // S-type Immediate
+                o_alu_op    = ALU_ADD; // rs1 + imm
             end
 
             // ------------------------- B-type -------------------------
-            7'b1100011: begin // Branch
-                o_branch  = 1'b1;    // Branch
-                o_imm_sel = 2'b10;   // B-type Immediate
-                o_alu_op  = 4'b0000; // ADD
+            7'b1100011: begin
+                o_branch    = TRUE;    // Branch
+                o_imm_sel   = IMM_B;   // B-type Immediate
+                o_alu_op    = ALU_SUB; // SUB
             end
 
-            // ------------------------- U-type -------------------------
+            // ------------------------- U-type (LUI) -------------------
+            7'b0110111: begin
+                o_reg_write = TRUE;    // Write Register
+                o_imm_sel   = IMM_U;   // U-type Immediate
+                o_alu_op    = ALU_ADD; // ADD
+            end
 
-            // ------------------------- J-type -------------------------
+            // ------------------------- U-type (AUIPC) -----------------
+            7'b0010111: begin
+                o_reg_write = TRUE;    // Write Register
+                o_imm_sel   = IMM_U;   // U-type Immediate
+                o_alu_op    = ALU_ADD; // ADD
+            end
+
+            // ------------------------- J-type (JAL) --------------------
+            7'b1101111: begin
+                o_reg_write = TRUE;    // Write Register
+                o_jump      = TRUE;    // Jump
+                o_imm_sel   = IMM_J;   //
+                o_alu_op    = ALU_ADD; //
+            end
+
+            // ------------------------- J-type (JALR) -------------------
+            7'b1100111: begin
+                o_reg_write = TRUE;    // Write Register
+                o_jump      = TRUE;    // Jump
+                o_imm_sel   = IMM_I;   //
+                o_alu_op    = ALU_ADD; //
+            end
 
             default: begin
-                temp;
+
             end
         endcase
     end
--- define.svh
+++ define.svh
@@ -8,13 +8,33 @@ parameter ALU_OP = 5;
 parameter OPCODE = 7;
 parameter FUNCT3 = 3;
 parameter FUNCT7 = 7;
-parameter IMM_SEL = 3; // I, S, B, U, J
+parameter IMM_SEL = 3;
+parameter TRUE  = 1;
+parameter FALSE = 0;
 
 // Parameter definitions
 parameter DATA_WIDTH = 32;
 parameter ADDR_WIDTH = 16;
 parameter CACHE_SIZE = 1024;
 
+// ALU Operations
+parameter ALU_ADD = 4'b0000;
+parameter ALU_SUB = 4'b0001;
+parameter ALU_AND = 4'b0010;
+parameter ALU_OR  = 4'b0011;
+parameter ALU_XOR = 4'b0100;
+parameter ALU_SLL = 4'b0101;
+parameter ALU_SRL = 4'b0110;
+parameter ALU_SRA = 4'b0111;
+parameter ALU_X   = 4'bxxxx;
+
+
+// Immediate Select
+paramemter IMM_I = 3'b000;
+paramemter IMM_S = 3'b001;
+paramemter IMM_B = 3'b010;
+paramemter IMM_U = 3'b011;
+paramemter IMM_J = 3'b100;
 
 // Macro definitions
 `define ENABLE  1'b1
